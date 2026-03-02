c8a58dae145116d2e0ef1eca90ceea874eab5271
unknown <sm@morikaglobal.com'git config --global user.name morikaq>
2026-03-02 12:47:51 +0900

    fixed additional module error

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
+# from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import load_qa_chain
 from langchain.prompts import PromptTemplate
 
 import os
