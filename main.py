c3d627358d7b638088ba4899060ff5d43ba2d28b
unknown <sm@morikaglobal.com'git config --global user.name morikaq>
2026-03-02 12:42:09 +0900

    fixed module error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
