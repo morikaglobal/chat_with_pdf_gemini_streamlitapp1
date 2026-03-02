2315eb97971f8613bc1635b1329a53bf67933631
unknown <sm@morikaglobal.com'git config --global user.name morikaq>
2026-03-02 13:04:46 +0900

    fixed additional module error

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-# from langchain.chains.question_answering import load_qa_chain
-# from langchain_classic.chains import load_qa_chain
 from langchain_classic.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+# from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 import os
 from dotenv import load_dotenv
